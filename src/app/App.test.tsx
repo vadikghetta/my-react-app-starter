efb5290c1c91b6a4cd58a03f767ce93a65b7ab9d
vadikghetta <122256298+vadikghetta@users.noreply.github.com>
2024-08-09 17:09:13 +0300

    add starting template

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Hello есть в приложении", async () => {
+test("Hello Its my react starter in app", async () => {
 	render(<App />);
-	const hello = screen.getByText("Hello");
+	const hello = screen.getByText("Hello Its my react starter");
 	expect(hello).toBeInTheDocument();
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import styles from "./App.module.scss";
 
 
 const App = () => {
-
+	const [count, setCount] = useState(0);
 	return (
 		<main className={styles.main}>
-			<h1>Hello</h1>
-
+			<h1 className={styles.title}>Hello Its my react starter</h1>
+			<img className={styles.logo} src="/assets/react_logo.png" alt="react" width={280} height={250} />
+			<div className={styles.counter}>
+				<p className={styles.count}>Count is {count}</p>
+				<button className={styles.btn} onClick={() => {
+					setCount(state => state + 1);
+				}}>Click</button>
+			</div>
 		</main>
 	);
 };
